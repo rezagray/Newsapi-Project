384688435f7f0b2b976e888065debcfdc16bddf5
Arifrififf <72956310+Arifrififf@users.noreply.github.com>
2021-04-26 12:50:25 +0700

    commit 2

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static String key = "";
+  static String secret = "";
+  static String url = "https://groceryapp.snippetcoder.com/wp-json/wc/v3/";
+  static String customerURL = "customers";
+}
---
+++ lib/utils/validator_service.dart
@@ -0,0 +1,8 @@
+
+extension EmailValidator on String {
+  bool isValidEmail() {
+    return RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(()))
+    )
+  }
+}
